04fff5afee44a6b4db271bbf8bd642c77d93c9c6
Alxndr Brgmnn <alxndr.wlfgng.brgmnn@gmail.com>
2026-04-06 19:18:33 +0200

    Aktualisieren von service_notifications.py

--- lib/service_notifications.py
+++ lib/service_notifications.py
@@ -14,7 +14,7 @@ def send_stop_notification(notify, hostname):
     Console.success("Service stopped successfully")
 
 
-def send_start_notification(cron_string=None, notify, hostname):
+def send_start_notification(notify, hostname, cron_string=None):
     now = time.strftime("%Y-%m-%d %H:%M:%S")
 
     if cron_string and cron_string.lower() != "false":
